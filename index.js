c265fb9b0cede61cf187126075283ca582cf9074
Young Ahn <young_ahn@yonsei.ac.kr>
2020-01-13 22:53:10 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// // 1-5 Hello World for JavaScript
+
+alert('I am working. I am JavaScript. I will work for my girlfriend too :)');
+
+console.log('I am working. I am JavaScript. I will work for my girlfriend too :)')
+
+// // 1-6 varible in JavaScript
+// // variable: not consistent or having a fixed pattern; liable to change.
+// we need to put let in front of the initialization for variable (or creation of variable)
+let a = 221;
+let b = a - 5;
+console.log(b,a)
+
+// 1-7 const, var
+
+// const is constant. The Value that doesn't change. 
+// let is value that could change. It is variable
+// just don't use var. Use either const or let. 
+
+
+/*
+Below example yields error Assignment to ...
+const a = 221;
+let b = a - 5;
+a = 4 
+console.log(b,a)
+*/
+
+
+// 1-8 Data Types on JavaScript
+
+// Use const as a default.  Use let, only when you need it. 
+
+// String is text.
+const what = "I love JavaScript"
+console.log(what)
+
+// Boolean is True or False.
+const wat = true; // true is 1, false is 0 in binary world. Make sure it is in lower case. 
+
+// Float is number with floating number
+const wat = 55.1;
+
+
+// 1-9 Organizing Data with Arrays
+// Before we begin, make sure to use Semicolons all the time.
+
+// Array is how we store data in least kind of way. 
+
+const daysOfWeek = []
