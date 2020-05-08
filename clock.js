0c41a73cf1cf464ff3e3b852e668344c0710fe32
noopy <young_ahn@yonsei.ac.kr>
2020-05-08 15:33:11 +0900

    displaying clock on template with javascript

--- clock.js
+++ clock.js
@@ -1,6 +1,20 @@
 // selecting class in html document
-const clockContainer = documenmt.querySelector(".js-clock"),
+const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
-function init() {}
+// accessing javascript built-in object Date()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// just like python init
+function init() {
+  getTime();
+}
+
 init();
