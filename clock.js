2ed3679b9c392efacd27d546d4a7e87c7dae26d6
noopy <young_ahn@yonsei.ac.kr>
2020-05-08 16:11:25 +0900

    formatting time and run the clock every 1000ms

--- clock.js
+++ clock.js
@@ -9,12 +9,18 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  //  time formatting as 12:12 is okay, but 12:9 is not okay -> setup to 12:09
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 // just like python init
 function init() {
+  // run the clock function
   getTime();
+  // run the clock function every 1000 microseconds
+  setInterval(getTime, 1000);
 }
 
 init();
