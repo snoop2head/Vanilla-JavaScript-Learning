8991c68517447582f659a54ec8e3c157428443f8
noopy <young_ahn@yonsei.ac.kr>
2020-05-10 01:08:07 +0900

    finalize name saving & displaying to greet user

--- greetings.js
+++ greetings.js
@@ -7,12 +7,31 @@ const form = document.querySelector(".js-form"), // <form class = "js-form">
 
 // designating default values for local storage
 const USER = "currentUser",
-  SHOWING_CN = "showing";
+  SHOWING_NAME = "showing";
+
+// saving inputted name
+function saveName(text) {
+  localStorage.setItem(USER, text);
+}
+
+// utilizing paintGreetings and saveName
+function handleSubmit(event) {
+  event.preventDefault(); // preventing default behavior to disappear inputted text
+  const currentValue = input.value; // designating inputted value
+  paintGreetings(currentValue);
+  saveName(currentValue);
+}
+
+// submitting the form of name
+function askForName() {
+  form.classList.add(SHOWING_NAME);
+  form.addEventListener("submit", handleSubmit);
+}
 
 // decorating function for greetings text
 function paintGreetings(text) {
-  form.classList.remove(SHOWING_CN); // hiding the input form for name
-  greetings.classList.add(SHOWING_CN);
+  form.classList.remove(SHOWING_NAME); // hiding the input form for name
+  greetings.classList.add(SHOWING_NAME);
   greetings.innerText = `Hello ${text}`;
 }
 
@@ -21,9 +40,10 @@ function loadName() {
   // get item from local storage
   const currentUser = localStorage.getItem(USER);
   if (currentUser === null) {
-    //  when user information does not exist
+    //  when user information does not exist, get his/her name
+    askForName();
   } else {
-    //  when user information exists
+    //  when user information present
     paintGreetings(currentUser);
   }
 }
