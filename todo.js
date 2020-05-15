5ca77a9ed749cd1944a02c183a4589076ab5a66e
noopy <young_ahn@yonsei.ac.kr>
2020-05-15 18:40:44 +0900

    fix saving on local storage action & added footnotes

--- todo.js
+++ todo.js
@@ -6,7 +6,60 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODO_LS = "toDos";
 
+// list of todos as empty array
 let toDos = [];
+
+// creates html elements, saves todo list in local storage as string
+function interactToDo(text) {
+  // creating list element in HTML with createElement()
+  const li = document.createElement("li");
+  // deleting button
+  const delBtn = document.createElement("button");
+  // creating span element
+  const span = document.createElement("span");
+  // new id to save as todo object
+  const newId = toDos.length + 1;
+  // designate values as innerText and add event listener
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  // putting delete button and span inside of list html element
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  // designating id for each list element
+  li.id = newId;
+  toDoList.appendChild(li);
+  // todo object structure
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  // adding element to array as push()
+  toDos.push(toDoObj);
+  // saving toDos on local storage using saveToDos()
+  saveToDos();
+}
+
+// saving toDos on local storage
+function saveToDos() {
+  // Json.stringify to make javascript object into string
+  // Javascript object is like a cabinet: https://javascript.info/object
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
+}
+
+// loading saved todos in local storage, restore them using intereactToDo()
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODO_LS);
+  if (loadedToDos !== null) {
+    // parsing string object -> load and make it as javascript object
+    const parsedToDos = JSON.parse(loadedToDos);
+    // on each elements in parsed array, it performs interactToDos()
+    parsedToDos.forEach(function (toDo) {
+      // commence interactToDo() on toDo array's element's text
+      interactToDo(toDo.text);
+    });
+  }
+}
 
 function deleteToDo(event) {
   const btn = event.target;
@@ -19,45 +72,11 @@ function deleteToDo(event) {
   saveToDos();
 }
 
-function saveToDos() {
-  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
-}
-
-function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", deleteToDo);
-  span.innerText = text;
-  li.appendChild(delBtn);
-  li.appendChild(span);
-  li.id = newId;
-  toDoList.appendChild(li);
-  const toDoObj = {
-    text: text,
-    id: newId,
-  };
-  toDos.push(toDoObj);
-  saveToDos;
-}
-
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
-  paintToDo(currentValue);
+  interactToDo(currentValue);
   toDoInput.value = "";
-}
-
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODO_LS);
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function (toDo) {
-      paintToDo(toDo.text);
-    });
-  }
 }
 
 function init() {
