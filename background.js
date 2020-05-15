a6040485e6328a0038f887f28ad7224b9940f522
noopy <young_ahn@yonsei.ac.kr>
2020-05-15 22:01:35 +0900

    add footnote to background.js

--- background.js
+++ background.js
@@ -5,11 +5,13 @@ const IMG_NUMBER = 3;
 function paintImage(imgNumber) {
   const image = new Image();
   image.src = `images/${imgNumber + 1}.png`;
+  // refer to index.css for background image
   image.classList.add("backgroundImage");
   body.prepend(image);
 }
 
 function genRandom() {
+  // Math class, number between 0 and 2
   const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
